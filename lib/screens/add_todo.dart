60051e3c61139e9980c996c5553ca243b66ffb3e
pauljsd <paul.atolagbe.ayobami@gmail.com>
2024-02-24 15:24:43 +0100

    created form to add new todo to the server

--- lib/screens/add_todo.dart
+++ lib/screens/add_todo.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class AddTodoPage extends StatefulWidget {
   const AddTodoPage({super.key});
@@ -8,6 +11,10 @@ class AddTodoPage extends StatefulWidget {
 }
 
 class _AddTodoPageState extends State<AddTodoPage> {
+  //create controller and attach to respective textfield
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +22,58 @@ class _AddTodoPageState extends State<AddTodoPage> {
         backgroundColor: Colors.blue,
         title: Text('Add Todo'),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView(
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(hintText: 'Title'),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              controller: descriptionController,
+              decoration: InputDecoration(hintText: 'Description'),
+              keyboardType: TextInputType.multiline,
+              minLines: 5,
+              maxLines: 8,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: submitData,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+              ),
+              child: Text(
+                'Submit',
+                style: TextStyle(fontSize: 20),
+              ),
+            )
+          ],
+        ),
+      ),
     );
+  }
+
+  //Form Handling
+  Future<void> submitData() async {
+    print('omo');
+    //get data from form
+    final title = titleController.text;
+    final description = descriptionController.text;
+    final body = {
+      "title": title,
+      "description": description,
+      "is_completed": false
+    };
+    //submit data to server
+    final url = 'https://api.nstack.in/v1/todos';
+    final uri = Uri.parse(url);
+    final response = await http.post(uri,
+        body: jsonEncode(body), headers: {'Content-Type': 'application/json'});
+    //show success or fail
+    print(response.statusCode);
+    print(response.body);
+    print('done o');
   }
 }
